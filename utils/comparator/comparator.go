298cff6a3ec9f8993d92b76db8d48eac9cd3903a
i9 <oluwarinolasam@gmail.com>
2024-06-17 07:39:35 +0100

    comparator adjusted

--- utils/comparator/comparator.go
+++ utils/comparator/comparator.go
@@ -1,43 +1,33 @@
 package comparator
 
-import "cmp"
-
-type cmprt struct {
+type Comparator struct {
 	Compare func(a, b any) int
 }
 
-func (c cmprt) Equal(a, b any) bool {
+func (c Comparator) Equal(a, b any) bool {
 	return c.Compare(a, b) == 0
 }
-func (c cmprt) LessThan(a, b any) bool {
+
+func (c Comparator) LessThan(a, b any) bool {
 	return c.Compare(a, b) < 0
 }
-func (c cmprt) GreaterThan(a, b any) bool {
+
+func (c Comparator) GreaterThan(a, b any) bool {
 	return c.Compare(a, b) > 0
 }
-func (c cmprt) LessThanOrEqual(a, b any) bool {
+
+func (c Comparator) LessThanOrEqual(a, b any) bool {
 	return c.LessThan(a, b) || c.Equal(a, b)
 }
-func (c cmprt) GreaterThanOrEqual(a, b any) bool {
+
+func (c Comparator) GreaterThanOrEqual(a, b any) bool {
 	return c.GreaterThan(a, b) || c.Equal(a, b)
 }
 
-func (c *cmprt) Reverse() {
+func (c *Comparator) Reverse() {
 	compareOriginal := c.Compare
 
 	c.Compare = func(a, b any) int {
 		return compareOriginal(b, a)
 	}
-}
-
-func defaultCompareFunction(a, b any) int {
-	return cmp.Compare(a.(int), b.(int))
-}
-
-func Comparator(compareFunction func(a, b any) int) cmprt {
-	if compareFunction == nil {
-		return cmprt{Compare: defaultCompareFunction}
-	}
-
-	return cmprt{Compare: compareFunction}
 }
